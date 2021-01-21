5ba3f283d0fd7bab1508b2cd729a3f0ab6c70631
DavidQF555 <davidfeng2003@gmail.com>
2021-01-21 00:15:46 -0800

    Initial Commit

---
+++ src/main/java/io/github/davidqf555/minecraft/cults/Cults.java
@@ -0,0 +1,40 @@
+package io.github.davidqf555.minecraft.cults;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("cults")
+public class Cults {
+
+    public static final String MOD_ID = "cults";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Cults() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event) {
+
+    }
+
+    private void processIMC(final InterModProcessEvent event) {
+    }
+}
